c7c1c2bfc470b2be3b1767e52c8587db89ef26a9
ttieman <tiemantanner@gmail.com>
2023-01-14 11:21:12 -0700

    added the choice prompts and combining sting function

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Random-password-generator
+
+## About
+    This is a random passowrd generator. When the user opens the 
+    website they will be prompted to input what character sets 
+    they would want involved in the password generated!
+
+## Function 
+
+You can choose how long your password is
+You can choose what characters you want
+You can generate a password to be used for any functional purpose 
+
+## Screenshot
+
+![alt = "A screenshot of the password generator"](./assets/images/passwordgeneratorscreenshot.png)
+
+## Link
+
+ [file:///C:/Users/oldon/bootcamp/class-challenges/module3/index.html](doc:linking-to-pages#file:///C:/Users/oldon/bootcamp/class-challenges/module3/index.html)
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); 
 
 var characterSets = {         //object defining character sets to be applied in password generation 
  numChars: "0123456789",  
@@ -16,28 +16,105 @@ isLowerCase: true,
 isNumberChars: true
 }
 
+var generationString = "" // this is the string that is updated with the users choices and allows for different string configurations
 
-function promptTheUser(){
-  userInput.passwordLength = prompt("How many characters should the password be?\nMinimum of 8 Characters.\nMaximum of 128 Characters.");
-  if (userInput.passwordLength < 8|userInput.passwordLength > 128 | ) // if statement checking user inputs
-  userInput.isSpecChars = prompt("Should it include special characters?\nTrue or False?");
-  userInput.isUpperCase = prompt("Should it include uppercase letters?\nTrue or False?");       //this function prompts the user then returns the updated values to the user input object
-  userInput.isLowerCase = prompt("Should it include lowercase letters?\nTrue or False?");
-  userInput.isNumberChars = prompt("Should it include numbers?\nTrue or False?");
+function lengthPrompt(){
+  var value = prompt("How many characters should the password be?\nMinimum of 8 Characters.\nMaximum of 128 Characters.");
+  if (value < 8 || value > 128 || value.isNaN){
+      alert("Please enter a value between 8 and 128!");
+      lengthPrompt();                                               
+  } else{
+    value = userInput.passwordLength;         // this determins the length of the string based on used input
+    return userInput.passwordLength;
+  }
+}
 
+function boolSpecChars() {
+ var specCharValue;
+ if(confirm("Should it include %Special Characters?\nTrue Or False?") === true){
+  specCharValue = true;
+  userInput.isSpecChars = specCharValue;
+  generationString = generationString + characterSets.specChars;
+  return generationString;                    
+}else{                                    // this determins wether or not to include special characters
+  specCharValue = false;
+  userInput.isSpecChars = specCharValue;            
+  return userInput.isSpecChars;
+}
+}
+
+function boolUpperCase(){
+  var upperCaseValue 
+  if(confirm("Should it include UPPERCASE letters?\nTrue Or False?") === true){
+    upperCaseValue = true;
+    userInput.isSpecChars = upperCaseValue;
+    generationString = generationString + characterSets.upperLetterChars
+    return generationString;
+  }else{                                        //this determins wether or not to include upper case letters
+    upperCaseValue = false;
+    userInput.isUpperCase = upperCaseValue;
+    return userInput.isUpperCase;
+  }
+}
+
+
+function boolLowerCase(){
+  var lowerCaseValue 
+  if(confirm("Should it include lowercase letters?\nTrue Or False?") === true){
+    lowerCaseValue = true;
+    generationString = generationString + characterSets.letterChars;
+    return generationString;
+  }else{
+    lowerCaseValue = false;                             //this determins wether or not to include lowercase case letters
+    userInput.isLowerCase = lowerCaseValue;
+    return userInput.isLowerCase;
+  }
+}
+
+function boolNumberChars(){
+  var numCharValue
+  if(confirm("Should it include #number characters?\nTrue Or False?") === true){
+    numCharValue = true;
+    userInput.isNumberChars = numCharValue;
+    generationString = generationString + characterSets.numChars;
+    return generationString;
+  }else{                                                          // determins wether or not to include number characters
+    numCharValue = false;
+    userInput.isNumberChars = numCharValue;
+    return userInput.isNumberChars;
+  }
+}
+
+function checkForString(){
+  if(generationString === ""){
+    alert("No Characters selected try again!")
+    promptTheUser();
+  }                                     // checks to make sure there is atleast one number string included and will reprompt the user if there isnt 
+  else{
+    return;
+  }
+}
+
+function promptTheUser(){  // this prompt handles user input and makes sure there are atleast one character set included 
+  lengthPrompt();
+  boolSpecChars();
+  boolUpperCase();
+  boolLowerCase();
+  boolNumberChars();
+  checkForString();
   return userInput;
 }
 
-function generatePassword(){
+function generatePassword(){           // this function generates the password 
   var text = "";
   for (let i = 0; i < userInput.passwordLength; i++) {
-    text += characterSets.upperLetterChars[(Math.floor(Math.random() * characterSets.upperLetterChars.length))];
+    text += generationString[(Math.floor(Math.random() * generationString.length))];
   }
   return text;
 }
 
 // Write password to the #password input
-function writePassword() {
+function writePassword() {            //this funtion writes the password into the html 
  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -45,9 +122,9 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);             
 
-console.log(userInput);
-console.log(characterSets);
-promptTheUser();
+console.log(userInput); // the consol.log commands were used for debugging purposes during the development of this application to track information
+console.log(characterSets);       
+promptTheUser(); // prompts the user when page is loaded 
 console.log(userInput);
